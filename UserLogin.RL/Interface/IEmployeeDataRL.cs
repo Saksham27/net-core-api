9b7075e7ed1c2eb9115b32652ad24f522f2e329f
Saksham27 <geekysaksham27@gmail.com>
2020-05-18 19:05:49 +0530

    Added: interface for Employee Repository

--- UserLogin.RL/Interface/IEmployeeDataRL.cs
+++ UserLogin.RL/Interface/IEmployeeDataRL.cs
@@ -8,7 +8,21 @@ namespace EmployeeRepository.Interface
 {
     public interface IEmployeeDataRL
     {
-        IEnumerable<EmployeeModel> GetAllEmployees();
-        Task<bool> Register(EmployeeModel model);
+
+
+        //Interface method for Employee Registration
+        int RegisterEmployee(EmployeeModel model);
+        //Interface method for Employee Login
+        int EmployeeLogin(LoginModel loginData);
+        //Interface method for delete Employee detail
+        int DeleteEmployee(EmployeeId Data);
+        //Interface method for update Employee detail
+        int UpdateEmployeeDetails(UpdateModel data);
+        //Interface method for get Employee detail 
+        IEnumerable<EmployeeModel> GetEmployeeDetails(string inputData);
+        //Interface method for get Employee detail 
+        IEnumerable<EmployeeModel> GetEmployeeDetailsWithId(EmployeeId inputData);
+        //Interface method for get all Employee detail
+        IEnumerable<EmployeeModel> GetAllEmployeeDetails();
     }
 }
