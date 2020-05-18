555f06451dfd07bb9ea5e10c34fb7af1c8cf4129
Saksham27 <geekysaksham27@gmail.com>
2020-05-18 19:06:56 +0530

    Added: interface for business layer

--- UserLogin.BL/Interface/IEmployeeDataBL.cs
+++ UserLogin.BL/Interface/IEmployeeDataBL.cs
@@ -1,14 +1,20 @@
 ﻿using CommonLayer.Model;
+using EmployeeRepository.CL.Model;
 using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace IEmployeeDataBL.Interface
+namespace BusinessLayer.Interface
 {
     public interface IEmployeeDataBL
     {
-        IEnumerable<EmployeeModel> GetAllEmployees();
-        Task<bool> Register(EmployeeModel model);
+        ResponseMessage GetAllEmployeeDetails();
+        ResponseMessage RegisterEmployee(EmployeeModel model);
+        ResponseMessage EmployeeLoginBL(LoginModel loginData);
+        ResponseMessage GetEmployeeDetails(string inputData);
+        ResponseMessage GetEmployeeDetailsWithId(EmployeeId inputData);
+        ResponseMessage UpdateEmployeeDetails(UpdateModel data);
+        ResponseMessage DeleteEmployee(EmployeeId id);
     }
 }
