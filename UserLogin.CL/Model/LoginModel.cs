1026835f507290091dcb7d434eab1b7364b4dc4f
Saksham27 <geekysaksham27@gmail.com>
2020-05-18 19:03:27 +0530

    Added: Poco class for Login

---
+++ UserLogin.CL/Model/LoginModel.cs
@@ -0,0 +1,32 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="LoginModel.cs" company="BridgeLabz Solutions LLP">
+//     Copyright (c) Company. All rights reserved.
+// </copyright>
+// <author> Saksham Singh </author>
+//-----------------------------------------------------------------------
+#region namespace CommonLayer.Model
+namespace CommonLayer.Model
+{
+    using System.ComponentModel.DataAnnotations;
+
+    #region LoginModel
+    /// <summary>
+    /// POCO class for Login
+    /// </summary>
+    public class LoginModel
+    {
+        /// <summary>
+        /// Gets or sets UserName
+        /// </summary>
+        [Required(ErrorMessage = "Username is required")]
+        public string UserName { get; set; }
+
+        /// <summary>
+        /// Gets or sets Password
+        /// </summary>
+        [Required(ErrorMessage = "Password Is Required")]
+        public string Password { get; set; }
+    }
+    #endregion
+}
+#endregion
