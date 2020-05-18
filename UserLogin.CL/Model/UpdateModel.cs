9d34d13fe3f09f00b81b3e32bd2110827105feab
Saksham27 <geekysaksham27@gmail.com>
2020-05-18 19:04:06 +0530

    Added: Poco class for Updating the employee details

---
+++ UserLogin.CL/Model/UpdateModel.cs
@@ -0,0 +1,46 @@
+﻿namespace CommonLayer.Model
+{
+    using System.ComponentModel;
+    using System.ComponentModel.DataAnnotations;
+
+    public class UpdateModel
+    {
+        /// <summary>
+        /// Gets or sets Employee Id
+        /// </summary>
+        [Required(ErrorMessage = "EmployeeId Is Required")]
+        public int EmployeeId { get; set; }
+
+        /// <summary>
+        /// Gets or sets FirstName
+        /// </summary>   
+        public string FirstName { get; set; }
+
+        /// <summary>
+        /// Gets or sets LastName
+        /// </summary>
+        public string LastName { get; set; }
+
+        /// <summary>
+        /// Gets or sets Email
+        /// </summary>
+        [EmailAddress]
+        public string Email { get; set; }
+
+        /// <summary>
+        /// Gets or sets UserName
+        /// </summary>
+        public string UserName { get; set; }
+
+        /// <summary>
+        /// Gets or sets Password
+        /// </summary>
+        [PasswordPropertyText]
+        public string Password { get; set; }
+
+        /// <summary>
+        /// Gets or sets City
+        /// </summary>
+        public string City { get; set; }
+    }
+}
