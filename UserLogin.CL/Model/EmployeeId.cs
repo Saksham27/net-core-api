fe8016698af88bd4810df5281106bcb9dee2f4b5
Saksham27 <geekysaksham27@gmail.com>
2020-05-18 19:03:01 +0530

    Added: Poco class for employee Id

---
+++ UserLogin.CL/Model/EmployeeId.cs
@@ -0,0 +1,19 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="EmployeeId.cs" company="BridgeLabz Solutions LLP">
+//     Copyright (c) Company. All rights reserved.
+// </copyright>
+// <author> Saksham Singh </author>
+//-----------------------------------------------------------------------
+namespace CommonLayer.Model
+{
+    /// <summary>
+    /// POCO class for Employee Id
+    /// </summary>
+    public class EmployeeId
+    {
+        /// <summary>
+        /// Gets or sets ID
+        /// </summary>
+        public int ID { get; set; }
+    } //// end : public class EmployeeId
+} //// end : namespace CommonLayer.Model
